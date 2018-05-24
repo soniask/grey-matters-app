e8497b5d17a2fa94e7b453d633a64091c2ef1289
Sonia Skarbek <sonia.skarbek@gmail.com>
2018-05-23 21:17:06 -0700

    Factor out ArticleView to be shared by Article and Home

---
+++ src/modules/articles/ArticleView.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-native';
+import ParsedText from 'react-native-parsed-text';
+import {
+  Text,
+  ScrollView,
+  View,
+  Image
+} from 'react-native';
+import { termsActions } from '../../actions';
+import styles from './ArticleStyles';
+import References from '../shared/References';
+import TermDialog from './TermDialog';
+
+class ArticleView extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	handleTermPress(term) {
+    let pattern = /<span>([\w\s]+)<\/span>/;
+    let match = term.match(pattern);
+    this.props.getTerms({ term: match[1] });
+  }
+
+  renderText(matchingString, matches) {
+    return `${matches[1]}`;
+  }
+
+	render() {
+    return (
+      <View>
+        <ScrollView>
+          <Image style={styles.image} source={{uri: 'http://greymattersjournal.com/wp-content/uploads/2018/01/HM-700x757.png'}}/>
+          <View style={styles.container}>
+            <Text style={styles.titleText}>
+              {this.props.content.title}
+            </Text>
+            <View style={styles.metaData}>
+              <View style={[styles.rightBorder, styles.metaDataBox]}>
+                <Text>AUTHOR</Text>
+                {/* <Text style={styles.blue}>{this.props.content.creators[0]}</Text> */}
+              </View>
+              <View style={[styles.rightBorder, styles.metaDataBox]}>
+                <Text>ARTIST</Text>
+                {/* <Text style={styles.blue}>{this.props.content.creators[0]}</Text> */}
+              </View>
+              <View style={[styles.metaDataBox]}>
+                <Text>{ new Date(this.props.content.publishTime).toLocaleDateString()}</Text>
+              </View>
+            </View>
+            <ParsedText
+              parse={
+                [
+                  {pattern: /<h2>([\S\s]+)<\/h2>/, style: styles.sectionTitle, renderText: this.renderText},
+                  {pattern: /<span>([\w\s]+)<\/span>/, style: styles.blue, onPress: (term) => this.handleTermPress(term), renderText: this.renderText},
+                ]
+              }
+            >
+              {this.props.content.body}
+            </ParsedText>
+            {this.props.content.references && <References references={this.props.content.references}/>}
+          </View>
+        </ScrollView>
+        {this.props.terms && <TermDialog terms={this.props.terms} />}
+      </View>
+    );
+	}
+}
+
+const mapStateToProps = state => ({
+  terms: state.terms.terms,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getTerms: termsActions.getTerms,
+  clearTerms: termsActions.clearTerms,
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ArticleView);
+  
