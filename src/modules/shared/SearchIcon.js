08277a76997a21240c66bec8b27d73fb9b12b3e8
Sonia Skarbek <sonia.skarbek@gmail.com>
2018-05-25 13:26:26 -0700

    Hide menu when user taps either the search or the settings icon

--- src/modules/shared/SearchIcon.js
+++ src/modules/shared/SearchIcon.js
@@ -10,9 +10,16 @@ import { Icon } from 'react-native-elements';
 import { menuActions } from '../../actions';
 
 class SearchIcon extends React.Component {
+  constructor(props) {
+	  super(props);
+  }
+  
   render () {
     return (
-      <Link to='/search'>
+      <Link 
+        to='/search'
+        onPress={() => this.props.showMenu(false)}
+      >
         <Icon 
           name='search' 
           color= '#282828'
@@ -22,4 +29,8 @@ class SearchIcon extends React.Component {
   }
 } 
 
-export default SearchIcon;
+const mapDispatchToProps = dispatch => bindActionCreators({
+  showMenu: menuActions.showMenu,
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(SearchIcon);
--- src/modules/shared/SettingsIcon.js
+++ src/modules/shared/SettingsIcon.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import {
   View,
   Text,
 } from 'react-native';
 import { Icon } from 'react-native-elements';
 import { Link } from 'react-router-native';
+import { menuActions } from '../../actions';
 
 class SettingsIcon extends React.Component {
+  constructor(props) {
+	  super(props);
+  }
+  
   render () {
     return (
-      <Link to='/settings'>
+      <Link 
+        to='/settings'
+        onPress={() => this.props.showMenu(false)}
+      >
         <Icon 
           name='settings'
           color= '#282828' />
@@ -18,4 +28,8 @@ class SettingsIcon extends React.Component {
   }
 } 
 
-export default SettingsIcon;
+const mapDispatchToProps = dispatch => bindActionCreators({
+  showMenu: menuActions.showMenu,
+}, dispatch);
+
+export default connect(null, mapDispatchToProps)(SettingsIcon);
