d9581dc7bf56a9753c873cfced9e75290d911c71
Sonia Skarbek <sonia.skarbek@gmail.com>
2018-05-27 14:43:24 -0700

    Show terms in alphabetical order

--- src/modules/terms/Terms.js
+++ src/modules/terms/Terms.js
@@ -35,6 +35,9 @@ class Terms extends Component {
         <Unavailable message='No terms available' />
       )
     }
+    this.props.terms.sort((term1, term2) => {
+      return term1.term.localeCompare(term2.term);
+    })
     return (
       <ScrollView>
         <View style={[styles.content]}>
